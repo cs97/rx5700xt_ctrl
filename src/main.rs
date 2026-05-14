b9a7dc964f4c13575d7877e9082df4f755994abd
cs97 <chrswr97@gmail.com>
2026-05-15 00:05:54 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,14 @@ const VC_1_MV: &str = "806";
 const ECO_VC_2_MHZ: &str = "1700";
 const ECO_VC_2_MV: &str = "875";
 
-const MAX_VC_2_MHZ: &str = "1900";
-const MAX_VC_2_MV: &str = "985";
+const LOW_VC_2_MHZ: &str = "1900";
+const LOW_VC_2_MV: &str = "985";
 
-const OC_VC_2_MHZ: &str = "2000";
-const OC_VC_2_MV: &str = "1050";
+const HIGH_VC_2_MHZ: &str = "2000";
+const HIGH_VC_2_MV: &str = "1050";
+
+const OC_VC_2_MHZ: &str = "2100";
+const OC_VC_2_MV: &str = "1100";
 
 fn set_vc(mode: &str) -> std::io::Result<()> {
 
@@ -30,35 +33,26 @@ fn set_vc(mode: &str) -> std::io::Result<()> {
 	match mode {
 
 		"eco" => {
-			// build vc
 			vc_1 = format!("vc 1 {} {}", VC_1_MHZ, VC_1_MV);
-
-			// build vc
 			vc_2 = format!("vc 2 {} {}", ECO_VC_2_MHZ, ECO_VC_2_MV);
-
-			// max freq
 			max_freq = ECO_VC_2_MHZ;
 		},
 
-		"max" => {
-			// build vc
+		"low" => {
 			vc_1 = format!("vc 1 {} {}", VC_1_MHZ, VC_1_MV);
+			vc_2 = format!("vc 2 {} {}", LOW_VC_2_MHZ, LOW_VC_2_MV);
+			max_freq = LOW_VC_2_MHZ;
+		},
 
-			// build vc
-			vc_2 = format!("vc 2 {} {}", MAX_VC_2_MHZ, MAX_VC_2_MV);
-
-			// max freq
+		"high" => {
+			vc_1 = format!("vc 1 {} {}", VC_1_MHZ, VC_1_MV);
+			vc_2 = format!("vc 2 {} {}", HIGH_VC_2_MHZ, HIGH_VC_2_MV);
 			max_freq = MAX_VC_2_MHZ;
 		},
 
 		"oc" => {
-			// build vc
 			vc_1 = format!("vc 1 {} {}", VC_1_MHZ, VC_1_MV);
-
-			// build vc
 			vc_2 = format!("vc 2 {} {}", OC_VC_2_MHZ, OC_VC_2_MV);
-
-			// max freq
 			max_freq = OC_VC_2_MHZ;
 		},
 
